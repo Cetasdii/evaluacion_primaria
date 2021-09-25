6e997be131618d072d15719364aa28d822de3d6c
Jennifer-Soto <79289377+Jennifer-Soto@users.noreply.github.com>
2021-09-25 15:56:53 -0500

    Add files via upload

---
+++ js/ejercicio1.js
@@ -0,0 +1,5 @@
+let usuario = prompt("Ingrese su nombre");
+let edad = parseInt(prompt("Ingrese su edad"));
+let imprimir = `Hola ${usuario}, su edad es ${edad} años`;
+
+console.log(imprimir);
---
+++ js/ejercicio2.js
@@ -0,0 +1,4 @@
+let primer_numero = 23;
+let segundo_numero = 33;
+
+//Si porque son variables diferentes
